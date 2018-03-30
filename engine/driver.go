87063303099afd2bc9cce6c8d3b23e8b0db3d66e
Sean Marciniak <a1668979@student.adelaide.edu.au>
2018-03-30 21:28:24 +0100

    Feature work on card processing (#1)
    * Adding the building blocks
    
    * Adding the start of the magical engine
    
    * Adding my current work on creating default store
    
    * Fixing file permissions
    
    * Replacing fmt with my logger
    
    * Fixing permission
    
    * Updating the Init Function
    
    * Adding basic check
    
    * Adding updates tot he Engine
    
    * Doing some refactoring
    
    * Made the refactor
    
    * Adding log update
    
    * Making some improvements

--- engine/driver.go
+++ engine/driver.go
@@ -1,14 +1,43 @@
 package engine
 
 import (
+	"encoding/json"
 	"errors"
+	"io/ioutil"
+	"os"
+	"path"
 	"reflect"
 
 	"github.com/RenegadeTech/MysticalTutor/interfaces"
+	"github.com/RenegadeTech/MysticalTutor/logger"
+	"github.com/RenegadeTech/MysticalTutor/types"
+)
+
+const (
+	// JsonCollectionURL is the URL used as the reference point for all cards available
+	JsonCollectionURL = "https://mtgjson.com/json/AllCards.json.zip"
+)
+
+var (
+	// CollectionPath is the local file directory as to where the "storage bank"
+	// of cards to use is kept. Can be changed incase the user wants to use a different sellection
+	// of cards
+	CollectionPath = "resources/AllCards.json"
 )
 
 type driver struct {
-	disp prototype.Display
+	disp        prototype.Display
+	store       types.Collection
+	collections []types.Collection
+	processors  []prototype.Processor
+}
+
+func New() prototype.Engine {
+	return &driver{
+		store:       types.Collection{},
+		collections: []types.Collection{},
+		processors:  []prototype.Processor{},
+	}
 }
 
 func (d *driver) Connect(disp prototype.Display) error {
@@ -25,5 +54,128 @@ func (d *driver) Connect(disp prototype.Display) error {
 }
 
 func (d *driver) Initialise() prototype.Engine {
+	logger.GetInstance().Log(logger.Entry{
+		Level: logger.Info,
+		Data:  "Initialising the engine",
+	})
+	d.store = types.Collection{}
+	_, err := os.Stat(CollectionPath)
+	switch {
+	case os.IsNotExist(err):
+		if err := d.downloadCardCollection(); err != nil {
+			logger.GetInstance().Log(logger.Entry{
+				Level: logger.Info,
+				Data:  "Recieved error: " + err.Error(),
+			})
+		}
+		fallthrough
+	default:
+		if err := d.loadCollectionFromDisk(); err != nil {
+			// Failed to load what we need
+			logger.GetInstance().Log(logger.Entry{
+				Level: logger.Fatal,
+				Data:  "Recieved error: " + err.Error(),
+			})
+		}
+	}
+	d.collections, d.processors = []types.Collection{}, []prototype.Processor{}
 	return d
+}
+
+func (d *driver) AddProcessor(p prototype.Processor) error {
+	if p == nil {
+		return errors.New("Process was nil")
+	}
+	d.processors = append(d.processors, p)
+	return nil
+}
+
+func (d *driver) RemoveProcessor(p prototype.Processor) error {
+	for i, proc := range d.processors {
+		if proc == p {
+			d.processors = append(d.processors[:i], d.processors[i+1:]...)
+			break
+		}
+	}
+	return nil
+}
+
+func (d *driver) AddCollection(c types.Collection) error {
+	if c == nil {
+		return errors.New("Collection was nil")
+	}
+	d.collections = append(d.collections, c)
+	return nil
+}
+
+func (d *driver) RemoveCollection(c types.Collection) error {
+	for i, collection := range d.collections {
+		if reflect.DeepEqual(c, collection) {
+			d.collections = append(d.collections[:i], d.collections[i+1:]...)
+			break
+		}
+	}
+	return nil
+}
+
+func (d *driver) GetProcessors() []prototype.Processor {
+	return d.processors
+}
+
+func (d *driver) GetCollections() []types.Collection {
+	return d.collections
+}
+
+func (d *driver) GetEntireCollection() types.Collection {
+	return d.store
+}
+
+func (d *driver) ProcessCollectionID(id int) {
+	// TODO(Sean Marciniak): Consider how we are going to process a given collection
+}
+
+func (d *driver) downloadCardCollection() error {
+	files, err := downloadZip(JsonCollectionURL)
+	if err != nil {
+		return err
+	}
+	for _, file := range files {
+		if file.FileInfo().IsDir() {
+			continue
+		}
+		f, err := file.Open()
+		if err != nil {
+			return err
+		}
+		defer f.Close()
+		b, err := ioutil.ReadAll(f)
+		if err != nil {
+			return err
+		}
+		if _, err = os.Stat(path.Dir(CollectionPath)); os.IsNotExist(err) {
+			os.MkdirAll(path.Dir(CollectionPath), 0744)
+		}
+		if err := ioutil.WriteFile(CollectionPath, b, 0644); err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (d *driver) loadCollectionFromDisk() error {
+	if _, err := os.Stat(CollectionPath); os.IsNotExist(err) {
+		return errors.New("File does not exist")
+	}
+	buff, err := ioutil.ReadFile(CollectionPath)
+	if err != nil {
+		return err
+	}
+	dto := map[string]*types.Card{}
+	if err := json.Unmarshal(buff, &dto); err != nil {
+		return err
+	}
+	for _, card := range dto {
+		d.store = append(d.store, card)
+	}
+	return nil
 }
--- interfaces/engine.go
+++ interfaces/engine.go
@@ -1,8 +1,27 @@
 package prototype
 
-// Engine ...
+import "github.com/RenegadeTech/MysticalTutor/types"
+
+// Engine allows for Factory like interactions
+// and well defined model processing and management
 type Engine interface {
 	Connect(display Display) error
 
 	Initialise() Engine
+
+	AddProcessor(p Processor) error
+
+	AddCollection(c types.Collection) error
+
+	RemoveProcessor(p Processor) error
+
+	RemoveCollection(c types.Collection) error
+
+	GetProcessors() []Processor
+
+	GetCollections() []types.Collection
+
+	GetEntireCollection() types.Collection
+
+	ProcessCollectionID(index int)
 }
